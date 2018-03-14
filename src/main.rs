834a8ae3f4cd0431862cb5f78b178d45d758f152
Narfinger <Narfinger@users.noreply.github.com>
2018-03-14 21:37:27 +0530

    now highlighting almost works

--- src/main.rs
+++ src/main.rs
@@ -36,7 +36,8 @@ macro_rules! clone {
 }
 
 type CurrentPlaylist = Arc<Mutex<playlist::Playlist>>;
-type Pipeline = Arc<Mutex<gstreamer::Element>>; 
+type Pipeline = Arc<Mutex<gstreamer::Element>>;
+type Gui = Arc<Mutex<gtk::Builder>>;
 
 /// poll the message bus and on eos start new
 fn gstreamer_message_handler(pipeline: Pipeline, current_playlist: CurrentPlaylist) {
@@ -88,6 +89,22 @@ fn gstreamer_init(current_playlist: CurrentPlaylist) -> Result<Pipeline> {
     Ok(p)
 }
 
+/// General purpose function to update the gui on any change
+fn update_gui(pipeline: Pipeline, playlist: CurrentPlaylist, gui: Gui) {
+    let (_, state, _) = pipeline.lock().unwrap().get_state(gstreamer::ClockTime(Some(1000)));  
+    let treeview: gtk::TreeView = gui.lock().unwrap().get_object("listview").unwrap();
+    let treeselection = treeview.get_selection();
+    if state == gstreamer::State::Paused || state == gstreamer::State::Playing {
+        let index = playlist.lock().unwrap().current_position;
+        let mut ipath = gtk::TreePath::new();
+        ipath.append_index(index as i32);
+        //let index = gtk::TreePath::new_from_indicesv([index]);
+        treeselection.select_path(&ipath);
+    } else {
+        treeselection.unselect_all();
+    }
+}
+
 fn main() {
     if gtk::init().is_err() {
         println!("Failed to initialize GTK.");
@@ -95,65 +112,79 @@ fn main() {
     }
 
     let glade_src = include_str!("../ui/main.glade");
-    let builder = gtk::Builder::new_from_string(glade_src);
+    let builder: Gui = Arc::new(Mutex::new(gtk::Builder::new_from_string(glade_src)));
 
     println!("Building list");
     let playlist = playlist::playlist_from_directory("/mnt/ssd-media/Musik/1rest/");
     let current_playlist = Arc::new(Mutex::new(playlist));
     println!("Done building list");
     
-    let window: gtk::Window = builder.get_object("mainwindow").unwrap();
-    
+    let window: gtk::Window = builder.lock().unwrap().get_object("mainwindow").unwrap();
+    let treeview: gtk::TreeView = builder.lock().unwrap().get_object("listview").unwrap();
+
     let pipeline = gstreamer_init(current_playlist.clone()).unwrap();
 
     
     { // Play Button
-        let button: gtk::Button = builder.get_object("playButton").unwrap();
-        button.connect_clicked(clone!(current_playlist, pipeline => move |_| {
-            let p = pipeline.lock().unwrap();
-            let pl = current_playlist.lock().unwrap();
-            (*p).set_property("uri", &playlist::get_current_uri(&pl));
-            p.set_state(gstreamer::State::Playing);
+        let button: gtk::Button = builder.lock().unwrap().get_object("playButton").unwrap();
+        button.connect_clicked(clone!(current_playlist, pipeline, builder => move |_| {
+            {
+                let p = pipeline.lock().unwrap();
+                let pl = current_playlist.lock().unwrap();
+                (*p).set_property("uri", &playlist::get_current_uri(&pl));
+                p.set_state(gstreamer::State::Playing); 
+            }
+            update_gui(pipeline.clone(), current_playlist.clone(), builder.clone());
         }));
     }
     { // Pause Button
-        let button: gtk::Button = builder.get_object("pauseButton").unwrap();
-        button.connect_clicked(clone!(pipeline => move |_| {
-            let p = pipeline.lock().unwrap();      
-            match p.get_state(gstreamer::ClockTime(Some(1000))) {
-                (_, gstreamer::State::Paused, _) =>  { (*p).set_state(gstreamer::State::Playing); },
-                (_, gstreamer::State::Playing, _) => { (*p).set_state(gstreamer::State::Paused);  },
-                (_, _, _) => {}
+        let button: gtk::Button = builder.lock().unwrap().get_object("pauseButton").unwrap();
+        button.connect_clicked(clone!(current_playlist, pipeline, builder  => move |_| {
+            {
+                let p = pipeline.lock().unwrap();      
+                match p.get_state(gstreamer::ClockTime(Some(1000))) {
+                    (_, gstreamer::State::Paused, _) =>  { (*p).set_state(gstreamer::State::Playing); },
+                    (_, gstreamer::State::Playing, _) => { (*p).set_state(gstreamer::State::Paused);  },
+                    (_, _, _) => {}
+                }
             }
+            update_gui(pipeline.clone(), current_playlist.clone(), builder.clone());
         }));
     }
     {  // Previous button
-        let button: gtk::Button = builder.get_object("prevButton").unwrap();
-        button.connect_clicked(clone!(current_playlist, pipeline => move |_| {
-            let p = pipeline.lock().unwrap();
-            let mut pl = current_playlist.lock().unwrap();
-            (*p).set_state(gstreamer::State::Paused);
-            (*p).set_state(gstreamer::State::Ready);
-            (*pl).current_position = ((*pl).current_position -1) % (*pl).items.len() as i64;
-            (*p).set_property("uri", &playlist::get_current_uri(&pl)).expect("Error in changing url");
-            (*p).set_state(gstreamer::State::Playing);
+        let button: gtk::Button = builder.lock().unwrap().get_object("prevButton").unwrap();
+        button.connect_clicked(clone!(current_playlist, pipeline, builder => move |_| {
+            {
+                let p = pipeline.lock().unwrap();
+                let mut pl = current_playlist.lock().unwrap();
+                (*p).set_state(gstreamer::State::Paused);
+                (*p).set_state(gstreamer::State::Ready);
+                (*pl).current_position = ((*pl).current_position -1) % (*pl).items.len() as i64;
+                (*p).set_property("uri", &playlist::get_current_uri(&pl)).expect("Error in changing url");
+                (*p).set_state(gstreamer::State::Playing);
+            }
+            update_gui(pipeline.clone(), current_playlist.clone(), builder.clone());
         }));
     }
     {  // Next button
-        let button: gtk::Button = builder.get_object("nextButton").unwrap();
-        button.connect_clicked(clone!(current_playlist, pipeline => move |_| {
-            let p = pipeline.lock().unwrap();
-            let mut pl = current_playlist.lock().unwrap();
-            (*p).set_state(gstreamer::State::Paused);
-            (*p).set_state(gstreamer::State::Ready);
-            (*pl).current_position = ((*pl).current_position +1) % (*pl).items.len() as i64;
-            (*p).set_property("uri", &playlist::get_current_uri(&pl)).expect("Error in changing url");
-            (*p).set_state(gstreamer::State::Playing);
+        let button: gtk::Button = builder.lock().unwrap().get_object("nextButton").unwrap();
+        button.connect_clicked(clone!(current_playlist, pipeline, builder => move |_| {
+            {
+                let p = pipeline.lock().unwrap();
+                let mut pl = current_playlist.lock().unwrap();
+                (*p).set_state(gstreamer::State::Paused);
+                (*p).set_state(gstreamer::State::Ready);
+                (*pl).current_position = ((*pl).current_position +1) % (*pl).items.len() as i64;
+                (*p).set_property("uri", &playlist::get_current_uri(&pl)).expect("Error in changing url");
+                (*p).set_state(gstreamer::State::Playing);
+            }
+            update_gui(pipeline.clone(), current_playlist.clone(), builder.clone());
         }));
     }
 
     let model = gtk::ListStore::new(&[u32::static_type(), String::static_type(), String::static_type(), String::static_type()]);
     
+    {
         let p = current_playlist.lock().unwrap();
         for (i, entry) in p.items.iter().enumerate() {
             let taglibfile = taglib::File::new(entry);
@@ -165,7 +196,6 @@ fn main() {
                     model.insert_with_values(None, &[0,1,2,3], &[&(i as u32 + 1), &tags.title(), &tags.artist(), &tags.album()]);
                 }
         }
-        let treeview: gtk::TreeView = builder.get_object("listview").unwrap();
         for id in vec![0,1,2,3] {
             let column = gtk::TreeViewColumn::new();
             let cell = gtk::CellRendererText::new();
@@ -175,7 +205,7 @@ fn main() {
             treeview.append_column(&column);
         }
         treeview.set_model(Some(&model));
-
+    }
     
     
     window.connect_delete_event(clone!(pipeline => move |_, _| {
