5e77fd037bbbd68d752b0bed1fa60b8bade25a27
Narfinger <Narfinger@users.noreply.github.com>
2018-08-31 00:08:12 +0530

    some more tries

--- src/dbus_adapter.rs
+++ src/dbus_adapter.rs
@@ -10,6 +10,7 @@ use std::sync::Arc;
 use std::fmt;
 
 use dbus_mpris_player::{OrgMprisMediaPlayer2Player, org_mpris_media_player2_player_server};
+use dbus_mpris_mediaplayer::{OrgMprisMediaPlayer2, org_mpris_media_player2_server};
 use gstreamer_wrapper::{GStreamerAction, GStreamerExt};
 use types::*;
 
@@ -180,15 +181,78 @@ impl OrgMprisMediaPlayer2Player for DBusAdapter {
 
 }
 
-fn create_iface() -> Interface<MTFn<TData>, TData> {
-    let f = tree::Factory::new_fn();
-    org_mpris_media_player2_player_server(&f, (), |m| {
-    // Just provide a link from MethodInfo (m) to the &Device
-    // we should call.
-    let a: &Arc<DBusAdapter> = m.path.get_data();
-    let b: &DBusAdapter = &a;
-    b
-    })
+struct DBusAdapter2 {
+    gstreamer: GStreamerPtr,
+}
+
+impl std::fmt::Debug for DBusAdapter2 {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Some gstreamer")
+    }
+}
+
+#[derive(Copy, Clone, Default, Debug)]
+struct TData2;
+impl tree::DataType for TData2 {
+    type Tree = ();
+    type ObjectPath = Arc<DBusAdapter2>;
+    type Property = ();
+    type Interface = ();
+    type Method = ();
+    type Signal = ();
+}
+
+
+impl OrgMprisMediaPlayer2 for DBusAdapter2 {
+    type Err = MethodErr;
+    fn raise(&self) -> Result<(), Self::Err> {
+        Ok(())
+    }
+
+    fn quit(&self) -> Result<(), Self::Err> {
+        Ok(())
+    }
+
+    fn get_can_quit(&self) -> Result<bool, Self::Err> {
+        Ok(false)
+    }
+
+    fn get_fullscreen(&self) -> Result<bool, Self::Err> {
+        Ok(false)
+    }
+
+    fn set_fullscreen(&self, value: bool) -> Result<(), Self::Err> {
+        Ok(())
+    }
+
+    fn get_can_set_fullscreen(&self) -> Result<bool, Self::Err> {
+        Ok(false)
+    }
+
+    fn get_can_raise(&self) -> Result<bool, Self::Err> {
+        Ok(false)
+    }
+
+    fn get_has_track_list(&self) -> Result<bool, Self::Err> {
+        Ok(false)
+    }
+
+    fn get_identity(&self) -> Result<String, Self::Err> {
+        Ok(String::from("Viola"))
+    }
+
+    fn get_desktop_entry(&self) -> Result<String, Self::Err> {
+        Ok(String::from(""))
+    }
+
+    fn get_supported_uri_schemes(&self) -> Result<Vec<String>, Self::Err> {
+        Ok(Vec::new())
+    }
+
+    fn get_supported_mime_types(&self) -> Result<Vec<String>, Self::Err> {
+        Ok(Vec::new())
+    }
+
 }
 
 fn create_tree(device: &Arc<DBusAdapter>, iface: &Arc<Interface<MTFn<TData>, TData>>) -> tree::Tree<MTFn<TData>, TData> {
@@ -201,16 +265,51 @@ fn create_tree(device: &Arc<DBusAdapter>, iface: &Arc<Interface<MTFn<TData>, TDa
     tree 
 }
 
+fn create_tree2(device: &Arc<DBusAdapter2>, iface: &Arc<Interface<MTFn<TData2>, TData2>>) -> tree::Tree<MTFn<TData2>, TData2> {
+    let f = tree::Factory::new_fn();
+    let mut tree = f.tree(());
+    tree = tree.add(f.object_path("/org/viola", device.clone())
+        .introspectable()
+        .add(iface.clone())
+        );
+    tree 
+}
+
+
 fn setup_single(gstreamer: GStreamerPtr) -> Result<(),String> {
     println!("setuing up dbus");
-    let dbusadapter = Arc::new(DBusAdapter {gstreamer: gstreamer });
-    let iface = create_iface();
-    let tree = create_tree(&dbusadapter, &Arc::new(iface));
-    
+    let tree = {
+        let dbusadapter = Arc::new(DBusAdapter {gstreamer: gstreamer.clone() });
+        let f = tree::Factory::new_fn();
+        let iface = org_mpris_media_player2_player_server(&f, (), |m| {
+            // Just provide a link from MethodInfo (m) to the &Device
+            // we should call.
+            let a: &Arc<DBusAdapter> = m.path.get_data();
+            let b: &DBusAdapter = &a;
+        b
+        });
+        create_tree(&dbusadapter, &Arc::new(iface))
+    };
+
+    let tree2 = {
+        let dbusadapter2 = Arc::new(DBusAdapter2 {gstreamer: gstreamer});
+        let f = tree::Factory::new_fn();
+        let iface2 = org_mpris_media_player2_server(&f, (), |m| {
+            let a: &Arc<DBusAdapter2> = m.path.get_data();
+            let b: &DBusAdapter2 = &a;
+            b
+        });
+        create_tree2(&dbusadapter2, &Arc::new(iface2))
+    };
     let c = Connection::get_private(BusType::Session).map_err(|_| String::from("Error in getting bus"))?;
     c.register_name("org.viola", 0).map_err(|_| String::from("could not register"))?;
     tree.set_registered(&c, true).map_err(|_| String::from("Could not register to tree"))?;
     c.add_handler(tree);
+    
+    c.register_name("org.viola", 0).map_err(|_| String::from("could not register"))?;
+    tree2.set_registered(&c, true).map_err(|_| String::from("Could not register to tree"))?;
+    c.add_handler(tree2);
+    
     gtk::idle_add(move || {
         c.incoming(10).next();
         gtk::Continue(true)
