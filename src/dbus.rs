29878e570db8991971945fe93b906e8dda12decc
Narfinger <Narfinger@users.noreply.github.com>
2019-08-21 18:24:49 +0530

    implementing dbus stubs

--- src/dbus.rs
+++ src/dbus.rs
@@ -9,17 +9,12 @@ struct DBusHandler {
 
 fn setup(gstreamer: Rc<GStreamer>) -> DBusHandler {
     let c = Connection::get_private(BusType::Session).expect("DBus error");
-    c.register_name("com.example.dbustest", NameFlag::ReplaceExisting as u32).expect("DBus error");
+    c.register_name("org.mpris.MediaPlayer2.viola", NameFlag::ReplaceExisting as u32).expect("DBus error");
     let f = Factory::new_fn::<()>();
-    let tree = f.tree(()).add(f.object_path("/hello", ()).introspectable().add(
-    f.interface("com.example.dbustest", ()).add_m(
-        f.method("Hello", (), |m| {
-            let n: &str = m.msg.read1()?;
-            let s = format!("Hello {}!", n);
-            Ok(vec!(m.msg.method_return().append1(s)))
-        }).inarg::<&str,_>("name")
-          .outarg::<&str,_>("reply")
-    )
+    let tree = f.tree(()).add(f.object_path("/org/mpris/MediaPlayer2", ()).introspectable().add(
+    f.interface("org.mpris.MediaPlayer2", ())
+    .add_m(f.method("Raise", (), |m| { Ok(vec!(m.msg.method_return())) }))
+    .add_m(f.method("Quit", (), |m|  { Ok(vec!(m.msg.method_return())) }))
     ));
     tree.set_registered(&c, true).expect("DBus error");
     c.add_handler(tree);
