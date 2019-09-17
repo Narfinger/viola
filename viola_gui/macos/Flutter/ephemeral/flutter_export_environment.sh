74f4fa95ea0716f190e68611077d924460c6dcad
Narfinger <narfinger@noreply.github.com>
2019-09-17 16:17:18 +0900

    some changes

--- viola_gui/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ viola_gui/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -2,8 +2,8 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/christianengels/GitHub/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/christianengels/GitHub/viola/viola_gui"
-export "FLUTTER_TARGET=/Users/christianengels/GitHub/viola/viola_gui/lib/main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_FRAMEWORK_DIR=/Users/christianengels/GitHub/flutter/bin/cache/artifacts/engine/darwin-x64"
+export "FLUTTER_FRAMEWORK_DIR=/Users/christianengels/GitHub/flutter/bin/cache/artifacts/engine/darwin-x64-release"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
