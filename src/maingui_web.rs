81dd134ee3e49b064541e58086b743c133f761fa
Narfinger <narfinger@noreply.github.com>
2019-11-27 19:41:43 +0900

    basic scaffolding of javascript stuff

--- src/maingui_web.rs
+++ src/maingui_web.rs
@@ -5,7 +5,8 @@ use crate::types::*;
 
 pub fn run(pool: &DBPool) {
     HttpServer::new(|| {
-        App::new().service(fs::Files::new("../web_gui/node_modules/", ".").show_files_listing())
+        App::new().service(fs::Files::new("/static/", "web_gui/dist/").show_files_listing())
+        .service(fs::Files::new("/", "./web_gui/").index_file("index.html"))
     })
     .bind("127.0.0.1:8088")
     .unwrap()
---
+++ web_gui/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+import ReactDOM from 'react-dom'
+import React from 'react'
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { liked: false };
+    }
+
+    render() {
+        if (this.state.liked) {
+            return 'You liked this.';
+        }
+
+        return e(
+            'button',
+            { onClick: () => this.setState({ liked: true }) },
+            'Like'
+        );
+    }
+}
+
+const domContainer = document.querySelector('#like_button_container');
+ReactDOM.render(e(LikeButton), domContainer);
