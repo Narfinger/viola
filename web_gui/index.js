deef42ced9d8e67a0aa8b7cbf61b773e212a1099
Narfinger <narfinger@noreply.github.com>
2019-11-28 16:36:13 +0900

    some more small stuff

--- web_gui/index.js
+++ web_gui/index.js
@@ -2,6 +2,7 @@
 
 import ReactDOM from 'react-dom'
 import React from 'react'
+import { FixedSizeGrid as Grid } from 'react-window';
 import axios from 'axios';
 const e = React.createElement;
 
@@ -12,8 +13,17 @@ var PlayState = {
 };
 
 
-function TransportButton(props) {
-    return <a href="#" onClick="axios.get(/api/transport/{props.api})">{props.title}</a>
+class TransportButton extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { title: "", url: "" }
+    }
+    click() {
+        axios.get("/transport/{this.state.url}");
+    }
+    render() {
+        return <button onClick={this.click}> {this.props.title}</button>
+    }
 }
 
 function PlayButton(props) {
@@ -26,9 +36,61 @@ function PlayButton(props) {
     if (props.play_state == PlayState.Playing) {
         return <TransportButton title="Pause" api="pause"></TransportButton>
     }
-    return <Button title="Unspecified"></Button>
+    return <TransportButton title="Unspecified" api="NI"></TransportButton>
 }
 
+function Main() {
+    return <div><div align="center">
+        <TransportButton title="Prev" api="prev"></TransportButton>
+        <PlayButton></PlayButton>
+        <TransportButton title="Next" api="next"></TransportButton>
+    </div>
+        <div>
+            <SongView></SongView>
+        </div>
+    </div>
+};
 
-const domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(e(LikeButton), domContainer);
+
+const Cell = ({ columnIndex, rowIndex, style }) => (
+    <div style={style}>
+        Item {rowIndex},{columnIndex}
+    </div>
+);
+
+class SongView extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { data: axios.get("/playlist/").then(| v | v).else([]) }
+    }
+
+    render() {
+        <Grid
+            columnCount={5}
+            columnWidth={this.data.length}
+            height={150}
+            rowCount={1000}
+            rowHeight={35}
+            width={300}
+        >
+            <Cell></Cell>
+        </Grid>
+
+    }
+}
+
+const Example = () => (
+    <List
+        height={700}
+        itemCount={10000}
+        itemSize={35}
+        width={800}
+    >
+        {Row}
+    </List>
+);
+
+
+
+const domContainer = document.querySelector('#main_container');
+ReactDOM.render(e(Main), domContainer);
