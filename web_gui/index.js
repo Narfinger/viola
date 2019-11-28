0a42e7a8ed8861353241e702db90749b880dc80a
Narfinger <narfinger@noreply.github.com>
2019-11-28 14:47:52 +0900

    jsx now works

--- web_gui/index.js
+++ web_gui/index.js
@@ -2,26 +2,33 @@
 
 import ReactDOM from 'react-dom'
 import React from 'react'
+import axios from 'axios';
 const e = React.createElement;
 
-class LikeButton extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { liked: false };
-    }
+var PlayState = {
+    Stopped: 1,
+    Paused: 2,
+    Playing: 3
+};
 
-    render() {
-        if (this.state.liked) {
-            return 'You liked this.';
-        }
 
-        return e(
-            'button',
-            { onClick: () => this.setState({ liked: true }) },
-            'Like'
-        );
-    }
+function TransportButton(props) {
+    return <a href="#" onClick="axios.get(/api/transport/{props.api})">{props.title}</a>
 }
+
+function PlayButton(props) {
+    if (props.play_state == PlayState.Stopped) {
+        return <TransportButton title="Play" api="play"></TransportButton>
+    };
+    if (props.play_state == PlayState.Paused) {
+        return <TransportButton title="Play" api="play"></TransportButton>
+    };
+    if (props.play_state == PlayState.Playing) {
+        return <TransportButton title="Pause" api="pause"></TransportButton>
+    }
+    return <Button title="Unspecified"></Button>
+}
+
 
 const domContainer = document.querySelector('#like_button_container');
 ReactDOM.render(e(LikeButton), domContainer);
--- web_gui/webpack.config.js
+++ web_gui/webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
+            presets: ['@babel/preset-react']
           }
         }
       }
