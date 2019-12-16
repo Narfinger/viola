9e3e8e2fac53c0761bcb1ce71db23323edc8aba3
Narfinger <narfinger@noreply.github.com>
2019-12-16 22:40:30 +0900

    trying to implement websockets

--- src/maingui_web.rs
+++ src/maingui_web.rs
@@ -1,13 +1,64 @@
+use actix::{Actor, StreamHandler};
+use actix::AsyncContext;
 use actix_files as fs;
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web_actors::ws;
 use std::sync::atomic::Ordering;
 use std::sync::Arc;
+use core::time::Duration;
+use std::sync::mpsc::{sync_channel, SyncSender, Receiver};
 
 use crate::gstreamer_wrapper;
 use crate::gstreamer_wrapper::GStreamerExt;
 use crate::playlist::restore_playlists;
 use crate::playlist_tabs;
 use crate::types::*;
+
+enum WsMessage {
+    PlayChanged,
+}
+
+struct MyWs {
+    recv: Receiver<WsMessage>,
+}
+
+impl Actor for MyWs {
+    type Context = ws::WebsocketContext<Self>;
+
+    fn started(&mut self, ctx: &mut Self::Context) {
+        self.sendmsg(ctx);
+    }
+}
+
+/// Handler for ws::Message message
+impl StreamHandler<ws::Message, ws::ProtocolError> for MyWs {
+    fn handle(&mut self, msg: ws::Message, ctx: &mut Self::Context) {
+        match msg {
+            ws::Message::Ping(msg) => ctx.pong(&msg),
+            ws::Message::Text(text) => ctx.text(text),
+            ws::Message::Binary(bin) => ctx.binary(bin),
+            _ => (),
+        }
+    }
+}
+
+impl MyWs {
+    fn sendmsg(&self, ctx: &mut <Self as Actor>::Context) {
+        ctx.run_interval(Duration::new(10, 0), |act, ctx| {
+            if let Ok(m) = self.recv.recv() {
+                match m {
+                    PlayChanged => ctx.send_text("playchanged");
+                }
+            }
+        });
+    }
+}
+
+fn ws_start(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
+    let resp = ws::start(MyWs {}, &req, stream);
+    println!("websocket {:?}", resp);
+    resp
+}
 
 async fn playlist(state: web::Data<WebGui>, req: HttpRequest) -> HttpResponse {
     HttpResponse::Ok().json(&state.playlist.items)
@@ -66,6 +117,7 @@ pub fn run(pool: DBPool) {
             .service(web::resource("/currentid/").route(web::get().to(current_id)))
             .service(web::resource("/transport/play").route(web::get().to(play)))
             .service(web::resource("/transport/pause").route(web::get().to(pause)))
+            .service(web::resource("/ws/").route(web::get().to(ws_start)))
             .service(fs::Files::new("/static/", "web_gui/dist/").show_files_listing())
             .service(fs::Files::new("/", "./web_gui/").index_file("index.html"))
     })
