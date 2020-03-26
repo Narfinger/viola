07e1a6058153b357f3d6a55e5d03275724338c0f
Narfinger <narfinger@noreply.github.com>
2020-03-26 14:12:42 +0900

    trying to convert to typescript

--- web_gui/webpack.config.js
+++ web_gui/webpack.config.js
@@ -1,11 +1,12 @@
 // webpack.config.js
 module.exports = {
   mode: 'development',
-  entry: './index.js',
+  entry: './index.tsx',
   output: {
     filename: 'main.js',
     publicPath: 'dist'
   },
+  devtool: "source-map",
   module: {
     rules: [
       {
@@ -19,7 +20,9 @@ module.exports = {
             }]
           }
         }
-      }
+      },
+      { test: /\.js$/, loader: "source-map-loader" },
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" }
     ]
   }
 };
