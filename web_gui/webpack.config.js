ba206e5c5bb445be20820f674c017de7ee5aa2a1
Narfinger <narfinger@noreply.github.com>
2020-03-26 14:19:52 +0900

    some more fiddling

--- web_gui/webpack.config.js
+++ web_gui/webpack.config.js
@@ -1,28 +1,42 @@
 // webpack.config.js
 module.exports = {
-  mode: 'development',
   entry: './index.tsx',
-  output: {
-    filename: 'main.js',
-    publicPath: 'dist'
-  },
+  mode: "production",
+
+  // Enable sourcemaps for debugging webpack's output.
   devtool: "source-map",
+
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx"]
+  },
+
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.ts(x?)$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-react', {
-              "plugins": ["@babel/plugin-proposal-class-properties"]
-            }]
+        use: [
+          {
+            loader: "awesome-typescript-loader"
           }
-        }
+        ]
       },
-      { test: /\.js$/, loader: "source-map-loader" },
-      { test: /\.tsx?$/, loader: "awesome-typescript-loader" }
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader"
+      }
     ]
+  },
+
+  // When importing a module whose path matches one of the following, just
+  // assume a corresponding global variable exists and use that instead.
+  // This is important because it allows us to avoid bundling all of our
+  // dependencies, which allows browsers to cache those libraries between builds.
+  externals: {
+    "react": "React",
+    "react-dom": "ReactDOM"
   }
 };
