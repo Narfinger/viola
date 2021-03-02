3ca20421f849979f04033e7bb5da2d2160e3fa1f
Narfinger <narfinger@noreply.github.com>
2021-03-02 12:18:12 +0900

    fix really stupid bug in libraryviewstore

--- src/maingui_web.rs
+++ src/maingui_web.rs
@@ -117,7 +117,7 @@ async fn library_load(
     _: HttpRequest,
 ) -> HttpResponse {
     let mut q = level.into_inner();
-    if q.search.is_some() && !q.search.as_ref().unwrap().is_empty() {
+    if q.search.is_some() && q.search.as_ref().unwrap().is_empty() {
         q.search = None;
     }
     let pl = libraryviewstore::load_query(&state.pool, &q);
