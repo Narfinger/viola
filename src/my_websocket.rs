393f2d61485f02c55322da07421d172ab52d81bb
Narfinger <narfinger@noreply.github.com>
2022-03-09 15:38:02 +0900

    close socket gracefully

--- src/my_websocket.rs
+++ src/my_websocket.rs
@@ -1,7 +1,4 @@
 use futures::{stream::SplitSink, SinkExt};
-//use actix::prelude::*;
-//use actix::{Actor, StreamHandler};
-//use actix_web_actors::ws;
 use std::{ops::DerefMut, sync::Arc};
 use tokio::sync::RwLock;
 use viola_common::WsMessage;
@@ -15,6 +12,10 @@ pub async fn send_my_message(socket: &MyWs, msg: WsMessage) {
     if let Some(sink) = socket.deref_mut() {
         let st = serde_json::to_string(&msg).expect("Error serializing");
         let msg = Message::text(&st);
-        sink.send(msg).await.expect("Error in sending");
+        if sink.send(msg).await.is_err() {
+	    info!("Closing Websocket");
+            sink.close().await.expect("Could not close socket");
+            socket.take();
+        }
     }
 }
