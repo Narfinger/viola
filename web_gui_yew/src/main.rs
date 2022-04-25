b5df28706695bc67e44fb8c7cd03bbf0330f41d8
Narfinger <narfinger@noreply.github.com>
2022-04-25 10:56:27 +0900

    more logs

--- web_gui_yew/src/main.rs
+++ web_gui_yew/src/main.rs
@@ -40,5 +40,7 @@ impl Component for Model {
 }
 
 fn main() {
+    wasm_logger::init(wasm_logger::Config::new(log::Level::Trace));
+    console_error_panic_hook::set_once();
     yew::start_app::<Model>();
 }
